d6933d876f4e85a882ffc4c773156ca32e24f390
Cezary Sanecki <saneckicezary@gmail.com>
2024-03-01 09:31:36 +0100

    Added spring boot base project

---
+++ src/test/java/pl/cezarysanecki/parkingdomain/ParkingDomainApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.cezarysanecki.parkingdomain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingDomainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
