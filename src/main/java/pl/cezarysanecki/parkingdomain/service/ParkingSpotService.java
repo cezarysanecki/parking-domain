7fb7275b4955515de58a8aef073e798a3d45bfe1
Cezary Sanecki <saneckicezary@gmail.com>
2024-03-02 18:26:56 +0100

    Add method to find parking spot

--- src/main/java/pl/cezarysanecki/parkingdomain/service/ParkingSpotService.java
+++ src/main/java/pl/cezarysanecki/parkingdomain/service/ParkingSpotService.java
@@ -21,5 +21,9 @@ public class ParkingSpotService {
         parkingSpot.setStatus(ParkingSpotStatus.AVAILABLE);
         return parkingSpotRepository.save(parkingSpot);
     }
+    public ParkingSpot findBy(Long id) {
+        return parkingSpotRepository.findById(id)
+                .orElseThrow(() -> new IllegalStateException("cannot find parking spot by id: " + id));
+    }
 
 }
