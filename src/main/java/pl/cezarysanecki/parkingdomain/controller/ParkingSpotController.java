4866ecb72ec16692fc7146f9ca2bf87d9ac71cbc
Cezary Sanecki <saneckicezary@gmail.com>
2024-03-04 14:56:54 +0100

    Added endpoints

--- src/main/java/pl/cezarysanecki/parkingdomain/controller/ParkingSpotController.java
+++ src/main/java/pl/cezarysanecki/parkingdomain/controller/ParkingSpotController.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.cezarysanecki.parkingdomain.model.ParkingSpot;
+import pl.cezarysanecki.parkingdomain.model.Vehicle;
 import pl.cezarysanecki.parkingdomain.service.ParkingSpotService;
+import pl.cezarysanecki.parkingdomain.service.VehicleService;
 
 import java.util.List;
 
@@ -17,6 +19,7 @@ import java.util.List;
 public class ParkingSpotController {
 
     private final ParkingSpotService parkingSpotService;
+    private final VehicleService vehicleService;
 
     @PostMapping
     public Long create() {
@@ -24,12 +27,33 @@ public class ParkingSpotController {
     }
 
     @PostMapping("/{id}/release")
-    public Long release(@PathVariable("id") Long id) {
+    public Long release(
+            @PathVariable("id") Long id
+    ) {
         return parkingSpotService.release(id).getId();
     }
 
+    @PostMapping("/{parkingSpotId}/reserve-for/{vehicleId}")
+    public Long reserve(
+            @PathVariable("parkingSpotId") Long parkingSpotId,
+            @PathVariable("vehicleId") Long vehicleId
+    ) {
+        Vehicle vehicle = vehicleService.findBy(vehicleId);
+        return parkingSpotService.reservedFor(parkingSpotId, vehicle).getId();
+    }
+
+    @PostMapping("/reserve-any/{vehicleId}")
+    public Long reserveAny(
+            @PathVariable("vehicleId") Long vehicleId
+    ) {
+        Vehicle vehicle = vehicleService.findBy(vehicleId);
+        return parkingSpotService.reservedAnyFor(vehicle).getId();
+    }
+
     @GetMapping("/{id}")
-    public ParkingSpot findBy(@PathVariable("id") Long id) {
+    public ParkingSpot findBy(
+            @PathVariable("id") Long id
+    ) {
         return parkingSpotService.findBy(id);
     }
 
