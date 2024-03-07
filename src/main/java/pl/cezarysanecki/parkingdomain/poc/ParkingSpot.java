edf9ebbefe953e589b36a61a88a39ce88e29a315
Cezary Sanecki <saneckicezary@gmail.com>
2024-03-07 10:19:24 +0100

    Added resolver

--- src/main/java/pl/cezarysanecki/parkingdomain/poc/ParkingSpot.java
+++ src/main/java/pl/cezarysanecki/parkingdomain/poc/ParkingSpot.java
@@ -1,7 +1,26 @@
 package pl.cezarysanecki.parkingdomain.poc;
 
+import java.util.Collection;
+import java.util.HashSet;
+
 interface ParkingSpot {
 
     ParkingSpotId parkingSpotId();
+
+    default ParkingSpot resolve(
+            ParkingSpotId parkingSpotId,
+            int capacity,
+            Collection<VehicleId> parkedVehicles) {
+        if (parkedVehicles.isEmpty()) {
+            return new FreeParkingSpot(parkingSpotId, capacity);
+        }
+        if (capacity > parkedVehicles.size()) {
+            return new PartiallyOccupiedParkingSpot(parkingSpotId, capacity, new HashSet<>(parkedVehicles));
+        }
+        if (capacity == parkedVehicles.size()) {
+            return new FullyOccupiedParkingSpot(parkingSpotId, capacity, new HashSet<>(parkedVehicles));
+        }
+        return new OverOccupiedParkingSpot(parkingSpotId, capacity, new HashSet<>(parkedVehicles));
+    }
 
 }
