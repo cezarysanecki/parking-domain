a6b207141aa7c20fd31500e516eae2dc889a8158
Cezary Sanecki <saneckicezary@gmail.com>
2024-03-07 13:34:05 +0100

    Fixed

--- src/main/java/pl/cezarysanecki/parkingdomain/availability/application/AssigningAvailableSpots.java
+++ src/main/java/pl/cezarysanecki/parkingdomain/availability/application/AssigningAvailableSpots.java
@@ -1,8 +1,8 @@
 package pl.cezarysanecki.parkingdomain.availability.application;
 
 import lombok.RequiredArgsConstructor;
-import pl.cezarysanecki.parkingdomain.availability.model.AssignedAvailableSpot;
-import pl.cezarysanecki.parkingdomain.availability.model.AvailableSpots;
+import pl.cezarysanecki.parkingdomain.availability.model.AssignedSpot;
+import pl.cezarysanecki.parkingdomain.availability.model.Spots;
 import pl.cezarysanecki.parkingdomain.commons.events.EventPublisher;
 import pl.cezarysanecki.parkingdomain.model.VehicleType;
 import pl.cezarysanecki.parkingdomain.parking.model.VehicleId;
@@ -10,17 +10,18 @@ import pl.cezarysanecki.parkingdomain.parking.model.VehicleId;
 @RequiredArgsConstructor
 public class AssigningAvailableSpots {
 
-    private final AvailableSpots availableSpots;
+    private final Spots spots;
     private final EventPublisher eventPublisher;
 
     public void assignTo(AssignmentCommand command) {
         VehicleId vehicleId = command.getVehicleId();
         VehicleType vehicleType = command.getVehicleType();
 
-        availableSpots.findAvailableFor(vehicleType)
-                .or(availableSpots::findAvailable)
+        spots.findAvailableFor(vehicleType)
+                .or(() -> spots.findAvailable()
+                        .map(free -> free.assignTo(vehicleType)))
                 .ifPresentOrElse(
-                        availableSpot -> eventPublisher.publish(new AssignedAvailableSpot(
+                        availableSpot -> eventPublisher.publish(new AssignedSpot(
                                 availableSpot.getParkingSpotId(), vehicleId)),
                         () -> {
                             throw new IllegalStateException("cannot find available spot for " + vehicleType);
--- src/main/java/pl/cezarysanecki/parkingdomain/availability/model/AssignedSpot.java
+++ src/main/java/pl/cezarysanecki/parkingdomain/availability/model/AssignedSpot.java
@@ -10,7 +10,7 @@ import java.time.Instant;
 import java.util.UUID;
 
 @Value
-public class AssignedAvailableSpot implements DomainEvent {
+public class AssignedSpot implements DomainEvent {
 
     @NonNull UUID eventId = UUID.randomUUID();
     @NonNull Instant when = Instant.now();
