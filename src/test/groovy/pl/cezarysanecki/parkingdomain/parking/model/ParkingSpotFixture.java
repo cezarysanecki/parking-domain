0074068e2643af41e123ac46f2511289c42c78a5
Cezary Sanecki <saneckicezary@gmail.com>
2024-03-08 22:55:38 +0100

    Added tests for releasing

--- src/test/groovy/pl/cezarysanecki/parkingdomain/parking/model/ParkingSpotFixture.java
+++ src/test/groovy/pl/cezarysanecki/parkingdomain/parking/model/ParkingSpotFixture.java
@@ -1,11 +1,16 @@
 package pl.cezarysanecki.parkingdomain.parking.model;
 
+import java.util.Set;
 import java.util.UUID;
 
 public class ParkingSpotFixture {
 
     public static ParkingSpot emptyWithCapacity(int capacity) {
         return new ParkingSpot(anyParkingSpotId(), capacity);
+    }
+
+    public static ParkingSpot parkingSpotWith(Vehicle vehicle) {
+        return new ParkingSpot(anyParkingSpotId(), vehicle.getVehicleSizeUnit().getValue(), Set.of(vehicle));
     }
 
     public static ParkingSpotId anyParkingSpotId() {
---
+++ src/test/groovy/pl/cezarysanecki/parkingdomain/parking/model/ReleasingParkingSpotTest.groovy
@@ -0,0 +1,46 @@
+package pl.cezarysanecki.parkingdomain.parking.model
+
+import io.vavr.control.Either
+import spock.lang.Specification
+
+import static pl.cezarysanecki.parkingdomain.parking.model.ParkingSpotEvent.*
+import static pl.cezarysanecki.parkingdomain.parking.model.ParkingSpotFixture.emptyWithCapacity
+import static pl.cezarysanecki.parkingdomain.parking.model.ParkingSpotFixture.parkingSpotWith
+import static pl.cezarysanecki.parkingdomain.parking.model.ParkingSpotFixture.vehicleWithSize
+
+class ReleasingParkingSpotTest extends Specification {
+  
+  def "can release parked vehicle"() {
+    given:
+      def vehicle = vehicleWithSize(1)
+    and:
+      def parkingSpot = parkingSpotWith(vehicle)
+    
+    when:
+      Either<ReleasingFailed, VehicleLeft> result = parkingSpot.release(vehicle.vehicleId)
+    
+    then:
+      result.isRight()
+      result.get().with {
+        assert it.parkingSpotId == parkingSpot.parkingSpotId
+        assert it.vehicle == vehicle
+      }
+  }
+  
+  def "vehicle cannot be release from parking spot if it is not on this spot"() {
+    given:
+      def parkingSpot = emptyWithCapacity(1)
+    and:
+      def vehicle = vehicleWithSize(1)
+    
+    when:
+      Either<ReleasingFailed, VehicleLeft> result = parkingSpot.release(vehicle.vehicleId)
+    
+    then:
+      result.isLeft()
+      result.getLeft().with {
+        assert it.parkingSpotId == parkingSpot.parkingSpotId
+      }
+  }
+  
+}
