1307114f1020dd728ae16bbf84c39087902971f1
Cezary Sanecki <saneckicezary@gmail.com>
2024-03-11 09:56:02 +0100

    Added helpers for aggregates

---
+++ src/main/java/pl/cezarysanecki/parkingdomain/commons/aggregates/Version.java
@@ -0,0 +1,12 @@
+package pl.cezarysanecki.parkingdomain.commons.aggregates;
+
+import lombok.Value;
+
+@Value
+public class Version {
+    int version;
+
+    public static Version zero() {
+        return new Version(0);
+    }
+}
