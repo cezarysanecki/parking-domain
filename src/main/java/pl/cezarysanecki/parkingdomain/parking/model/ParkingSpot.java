f4df7e088c1bace0a62771b5e0a46bc54f2342dd
Cezary Sanecki <saneckicezary@gmail.com>
2024-03-19 09:58:13 +0100

    Added default methods

--- src/main/java/pl/cezarysanecki/parkingdomain/parking/model/ParkingSpot.java
+++ src/main/java/pl/cezarysanecki/parkingdomain/parking/model/ParkingSpot.java
@@ -4,4 +4,12 @@ public interface ParkingSpot {
 
     ParkingSpotBase getBase();
 
+    default boolean isEmpty() {
+        return getBase().isEmpty();
+    }
+
+    default boolean isFull() {
+        return getBase().isFull();
+    }
+
 }
--- src/main/java/pl/cezarysanecki/parkingdomain/parking/model/ParkingSpotBase.java
+++ src/main/java/pl/cezarysanecki/parkingdomain/parking/model/ParkingSpotBase.java
@@ -1,11 +1,6 @@
 package pl.cezarysanecki.parkingdomain.parking.model;
 
 import lombok.Value;
-import pl.cezarysanecki.parkingdomain.parking.model.ParkedVehicles;
-import pl.cezarysanecki.parkingdomain.parking.model.ParkingSpotCapacity;
-import pl.cezarysanecki.parkingdomain.parking.model.ParkingSpotId;
-import pl.cezarysanecki.parkingdomain.parking.model.Vehicle;
-import pl.cezarysanecki.parkingdomain.parking.model.VehicleId;
 
 @Value
 public class ParkingSpotBase {
@@ -25,6 +20,14 @@ public class ParkingSpotBase {
 
     public boolean isFullyOccupiedWith(Vehicle vehicle) {
         return parkedVehicles.occupation() + vehicle.getVehicleSizeUnit().getValue() == capacity.getValue();
+    }
+
+    public boolean isEmpty() {
+        return parkedVehicles.isEmpty();
+    }
+
+    public boolean isFull() {
+        return parkedVehicles.occupation() == capacity.getValue();
     }
 
 }
