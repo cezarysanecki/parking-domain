a658f29932ef010414211cbcc38296d018fa20bc
Cezary Sanecki <saneckicezary@gmail.com>
2024-03-21 15:55:03 +0100

    Added type for park command

--- src/main/java/pl/cezarysanecki/parkingdomain/parking/application/parking/ParkVehicleCommand.java
+++ src/main/java/pl/cezarysanecki/parkingdomain/parking/application/parking/ParkVehicleCommand.java
@@ -3,12 +3,14 @@ package pl.cezarysanecki.parkingdomain.parking.application.parking;
 import lombok.NonNull;
 import lombok.Value;
 import pl.cezarysanecki.parkingdomain.parking.model.ParkingSpotId;
+import pl.cezarysanecki.parkingdomain.parking.model.ParkingSpotType;
 import pl.cezarysanecki.parkingdomain.parking.model.Vehicle;
 
 @Value
 public class ParkVehicleCommand {
 
     @NonNull ParkingSpotId parkingSpotId;
+    @NonNull ParkingSpotType parkingSpotType;
     @NonNull Vehicle vehicle;
 
 }
