2f7b93fc7324890c03d4234c63371ce2d1941c0d
Cezary Sanecki <saneckicezary@gmail.com>
2024-03-27 23:29:54 +0100

    Fixed test

--- src/test/groovy/pl/cezarysanecki/parkingdomain/client/reservationrequest/application/CancellingReservationRequestTest.groovy
+++ src/test/groovy/pl/cezarysanecki/parkingdomain/client/reservationrequest/application/CancellingReservationRequestTest.groovy
@@ -1,11 +1,14 @@
 package pl.cezarysanecki.parkingdomain.client.reservationrequest.application
 
 import io.vavr.control.Option
+import pl.cezarysanecki.parkingdomain.client.reservationrequest.infrastructure.ClientReservationsConfig
 import pl.cezarysanecki.parkingdomain.client.reservationrequest.model.ClientId
 import pl.cezarysanecki.parkingdomain.client.reservationrequest.model.ClientReservationRequests
 import pl.cezarysanecki.parkingdomain.client.reservationrequest.model.ClientReservationRequestsEvent
 import pl.cezarysanecki.parkingdomain.client.reservationrequest.model.ClientReservationRequestsRepository
 import pl.cezarysanecki.parkingdomain.commons.commands.Result
+import pl.cezarysanecki.parkingdomain.commons.date.LocalDateProvider
+import pl.cezarysanecki.parkingdomain.commons.events.EventPublisher
 import pl.cezarysanecki.parkingdomain.reservation.model.ReservationId
 import spock.lang.Specification
 
@@ -23,44 +26,48 @@ class CancellingReservationRequestTest extends Specification {
   
   LocalDateTime now = LocalDateTime.now()
   
+  EventPublisher eventPublisher = Mock()
   ClientReservationRequestsRepository repository = Stub()
+  
+  ClientReservationsConfig clientReservationsConfig = new ClientReservationsConfig(
+      eventPublisher, new LocalDateProvider())
+  
+  CancellingReservationRequest cancellingReservationRequest = clientReservationsConfig.cancellingReservationRequest(repository)
   
   def 'should successfully cancel reservation request for parking spot if there is one'() {
     given:
-      CancellingReservationRequest cancellingReservationRequest = new CancellingReservationRequest(repository)
-    and:
       persisted(reservationRequestsWith(clientId, reservationId, now))
     
     when:
-      def result = cancellingReservationRequest.cancelRequest(new CancelReservationRequestCommand(reservationId))
+      def result = cancellingReservationRequest.cancelRequest(new CancelReservationRequestCommand(reservationId, now))
     
     then:
       result.isSuccess()
-      result.get() == Result.Success
+      result.get() in Result.Success
   }
   
   def 'should reject cancelling reservation request for parking spot if there is any'() {
     given:
-      CancellingReservationRequest cancellingReservationRequest = new CancellingReservationRequest(repository)
+      CancellingReservationRequest cancellingReservationRequest = clientReservationsConfig.cancellingReservationRequest(repository)
     and:
       persisted(noReservationRequests(clientId, now))
     
     when:
-      def result = cancellingReservationRequest.cancelRequest(new CancelReservationRequestCommand(reservationId))
+      def result = cancellingReservationRequest.cancelRequest(new CancelReservationRequestCommand(reservationId, now))
     
     then:
       result.isSuccess()
-      result.get() == Result.Rejection
+      result.get() in Result.Rejection
   }
   
   def 'should fail if client reservations do not exist'() {
     given:
-      CancellingReservationRequest cancellingReservationRequest = new CancellingReservationRequest(repository)
+      CancellingReservationRequest cancellingReservationRequest = clientReservationsConfig.cancellingReservationRequest(repository)
     and:
       unknownClientReservationRequests()
     
     when:
-      def result = cancellingReservationRequest.cancelRequest(new CancelReservationRequestCommand(reservationId))
+      def result = cancellingReservationRequest.cancelRequest(new CancelReservationRequestCommand(reservationId, now))
     
     then:
       result.isFailure()
