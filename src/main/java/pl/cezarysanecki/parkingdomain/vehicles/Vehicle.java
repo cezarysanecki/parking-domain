6ba42afe44c480e41a2a3022c9f0ed8e9ba66a90
Cezary Sanecki <saneckicezary@gmail.com>
2024-03-29 12:52:16 +0100

    Prepared vehicle object

---
+++ src/main/java/pl/cezarysanecki/parkingdomain/vehicles/Vehicle.java
@@ -0,0 +1,11 @@
+package pl.cezarysanecki.parkingdomain.vehicles;
+
+import lombok.NonNull;
+
+@NonNull
+public class Vehicle {
+
+    @NonNull VehicleId vehicleId;
+    @NonNull VehicleSize vehicleSize;
+
+}
