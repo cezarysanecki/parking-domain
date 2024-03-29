eb2783d8f1f33581ed899f34338c429cb42a9a7a
Cezary Sanecki <saneckicezary@gmail.com>
2024-03-29 16:50:38 +0100

    Cleaned a lot

--- src/main/java/pl/cezarysanecki/parkingdomain/parkingspot/view/web/ParkingSpotController.java
+++ src/main/java/pl/cezarysanecki/parkingdomain/parkingspot/view/web/ParkingSpotController.java
@@ -1,30 +1,22 @@
 package pl.cezarysanecki.parkingdomain.parkingspot.view.web;
 
-import io.vavr.control.Try;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import pl.cezarysanecki.parkingdomain.commons.commands.Result;
-import pl.cezarysanecki.parkingdomain.parkingspot.parking.application.CreatingParkingSpot;
-import pl.cezarysanecki.parkingdomain.parkingspot.parking.model.ParkingSpotCapacity;
 import pl.cezarysanecki.parkingdomain.parkingspot.view.model.ParkingSpotView;
 import pl.cezarysanecki.parkingdomain.parkingspot.view.model.ParkingSpotViews;
 
 import java.util.Set;
 import java.util.UUID;
 
-import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 import static org.springframework.http.ResponseEntity.ok;
 
 @RestController
 @RequiredArgsConstructor
 class ParkingSpotController {
 
-    private final CreatingParkingSpot creatingParkingSpot;
     private final ParkingSpotViews parkingSpotViews;
 
     @GetMapping("/parking-spot/available")
@@ -32,21 +24,6 @@ class ParkingSpotController {
         return ok(parkingSpotViews.queryForAvailableParkingSpots());
     }
 
-    @PostMapping("/parking-spot/create")
-    ResponseEntity create(@RequestBody CreateParkingSpotRequest request) {
-        Try<Result> result = creatingParkingSpot.create(new CreatingParkingSpot.Command(
-                ParkingSpotCapacity.of(request.capacity)
-        ));
-        return result
-                .map(success -> ResponseEntity.ok().build())
-                .getOrElse(ResponseEntity.status(INTERNAL_SERVER_ERROR).build());
-    }
-
-}
-
-@Data
-class CreateParkingSpotRequest {
-    Integer capacity;
 }
 
 @Data
