77811e6de094d5a1f4a1cdb7287d358ad92241d6
Cezary Sanecki <saneckicezary@gmail.com>
2024-03-31 23:08:32 +0200

    Prepared client module for reservation

---
+++ src/main/java/pl/cezarysanecki/parkingdomain/reservation/client/model/ClientEvent.java
@@ -0,0 +1,25 @@
+package pl.cezarysanecki.parkingdomain.reservation.client.model;
+
+import lombok.NonNull;
+import lombok.Value;
+
+public interface ClientEvent {
+
+    ClientId getClientId();
+
+    @Value
+    class ReservationRequestSubmitted implements ClientEvent {
+
+        @NonNull ClientId clientId;
+        @NonNull ReservationId reservationId;
+
+    }
+
+    @Value
+    class ReservationRequestSubmissionFailed implements ClientEvent {
+
+        @NonNull ClientId clientId;
+
+    }
+
+}
