db61d7e3723075a0c4ea05fef841bc9a6a941aa7
Cezary Sanecki <saneckicezary@gmail.com>
2024-04-04 15:57:08 +0200

    Changed naming of service

--- src/main/java/pl/cezarysanecki/parkingdomain/reservation/client/infrastucture/ClientReservationsConfig.java
+++ src/main/java/pl/cezarysanecki/parkingdomain/reservation/client/infrastucture/ClientReservationsConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Profile;
 import pl.cezarysanecki.parkingdomain.commons.events.EventPublisher;
 import pl.cezarysanecki.parkingdomain.reservation.client.application.CancellingReservationRequest;
 import pl.cezarysanecki.parkingdomain.reservation.client.application.ParkingSpotReservationsEventHandler;
-import pl.cezarysanecki.parkingdomain.reservation.client.application.SubmittingReservationRequestForChosenParkingSpot;
+import pl.cezarysanecki.parkingdomain.reservation.client.application.ReservingPartOfParkingSpot;
 import pl.cezarysanecki.parkingdomain.reservation.client.model.ClientReservationsRepository;
 
 @Configuration
@@ -17,8 +17,8 @@ public class ClientReservationsConfig {
     private final EventPublisher eventPublisher;
 
     @Bean
-    SubmittingReservationRequestForChosenParkingSpot submittingReservationRequestForChosenParkingSpot(ClientReservationsRepository clientReservationsRepository) {
-        return new SubmittingReservationRequestForChosenParkingSpot(clientReservationsRepository);
+    ReservingPartOfParkingSpot reservingPartOfParkingSpot(ClientReservationsRepository clientReservationsRepository) {
+        return new ReservingPartOfParkingSpot(clientReservationsRepository);
     }
 
     @Bean
