e73e95ff1672fadb7b9a6b1669282f5c923be123
Cezary Sanecki <saneckicezary@gmail.com>
2024-04-07 23:24:15 +0200

    Added another unit test

--- src/test/groovy/pl/cezarysanecki/parkingdomain/reservation/client/model/ClientReservationsTest.groovy
+++ src/test/groovy/pl/cezarysanecki/parkingdomain/reservation/client/model/ClientReservationsTest.groovy
@@ -2,6 +2,7 @@ package pl.cezarysanecki.parkingdomain.reservation.client.model
 
 
 import pl.cezarysanecki.parkingdomain.parking.parkingspot.model.ParkingSpotId
+import pl.cezarysanecki.parkingdomain.parking.vehicle.model.VehicleSize
 import spock.lang.Specification
 
 class ClientReservationsTest extends Specification {
@@ -22,6 +23,25 @@ class ClientReservationsTest extends Specification {
       result.get().with {
         it.clientId == clientId
         it.parkingSpotId == parkingSpotId
+      }
+  }
+  
+  def "allow to reserve part of parking spot when client does not have any active reservation"() {
+    given:
+      def clientReservations = new ClientReservations(clientId, Set.of())
+    and:
+      def parkingSpotId = ParkingSpotId.newOne()
+      def vehicleSize = VehicleSize.of(2)
+      
+    when:
+      def result = clientReservations.createRequest(parkingSpotId, vehicleSize)
+    
+    then:
+      result.isRight()
+      result.get().with {
+        it.clientId == clientId
+        it.parkingSpotId == parkingSpotId
+        it.vehicleSize == vehicleSize
       }
   }
   
