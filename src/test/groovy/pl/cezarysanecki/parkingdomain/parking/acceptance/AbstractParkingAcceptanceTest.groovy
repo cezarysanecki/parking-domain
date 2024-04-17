91f82a8b75b362f0d744242058315f3cbba2213e
Cezary Sanecki <saneckicezary@gmail.com>
2024-04-17 16:22:55 +0200

    Extracted abstract parking acceptance test

---
+++ src/test/groovy/pl/cezarysanecki/parkingdomain/parking/acceptance/AbstractParkingAcceptanceTest.groovy
@@ -0,0 +1,46 @@
+package pl.cezarysanecki.parkingdomain.parking.acceptance
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.test.context.ActiveProfiles
+import pl.cezarysanecki.parkingdomain.commons.commands.Result
+import pl.cezarysanecki.parkingdomain.commons.events.EventPublisherTestConfig
+import pl.cezarysanecki.parkingdomain.parking.ParkingConfig
+import pl.cezarysanecki.parkingdomain.parking.parkingspot.application.CreatingParkingSpot
+import pl.cezarysanecki.parkingdomain.parking.parkingspot.model.ParkingSpotCapacity
+import pl.cezarysanecki.parkingdomain.parking.parkingspot.model.ParkingSpotCategory
+import pl.cezarysanecki.parkingdomain.parking.parkingspot.model.ParkingSpotId
+import pl.cezarysanecki.parkingdomain.parking.vehicle.application.RegisteringVehicle
+import pl.cezarysanecki.parkingdomain.parking.vehicle.model.VehicleId
+import pl.cezarysanecki.parkingdomain.parking.vehicle.model.VehicleSize
+import pl.cezarysanecki.parkingdomain.reserving.parkingspot.application.ParkingSpotReservationsFinder
+import spock.lang.Specification
+
+@ActiveProfiles("local")
+@SpringBootTest(classes = [
+    ParkingConfig.class,
+    EventPublisherTestConfig.class])
+abstract class AbstractParkingAcceptanceTest extends Specification {
+  
+  @MockBean
+  ParkingSpotReservationsFinder parkingSpotReservationsFinder
+  
+  @Autowired
+  RegisteringVehicle registeringVehicle
+  @Autowired
+  CreatingParkingSpot creatingParkingSpot
+  
+  ParkingSpotId createParkingSpot(int capacity) {
+    def result = creatingParkingSpot.create(new CreatingParkingSpot.Command(
+        ParkingSpotCapacity.of(capacity), ParkingSpotCategory.Gold))
+    return (result.get() as Result.Success<ParkingSpotId>).getResult()
+  }
+  
+  VehicleId registerVehicle(int size) {
+    def result = registeringVehicle.register(new RegisteringVehicle.Command(VehicleSize.of(size)))
+    return (result.get() as Result.Success<VehicleId>).getResult()
+  }
+  
+  
+}
--- src/test/groovy/pl/cezarysanecki/parkingdomain/parking/acceptance/AllowingToParkVehicleAcceptanceTest.groovy
+++ src/test/groovy/pl/cezarysanecki/parkingdomain/parking/acceptance/AllowingToParkVehicleAcceptanceTest.groovy
@@ -1,36 +1,14 @@
 package pl.cezarysanecki.parkingdomain.parking.acceptance
 
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.mock.mockito.MockBean
-import org.springframework.test.context.ActiveProfiles
-import pl.cezarysanecki.parkingdomain.commons.commands.Result
-import pl.cezarysanecki.parkingdomain.commons.events.EventPublisherTestConfig
-import pl.cezarysanecki.parkingdomain.parking.ParkingConfig
-import pl.cezarysanecki.parkingdomain.parking.parkingspot.application.CreatingParkingSpot
-import pl.cezarysanecki.parkingdomain.parking.parkingspot.model.ParkingSpotCapacity
-import pl.cezarysanecki.parkingdomain.parking.parkingspot.model.ParkingSpotCategory
 import pl.cezarysanecki.parkingdomain.parking.parkingspot.model.ParkingSpotId
 import pl.cezarysanecki.parkingdomain.parking.vehicle.application.ParkingVehicle
-import pl.cezarysanecki.parkingdomain.parking.vehicle.application.RegisteringVehicle
 import pl.cezarysanecki.parkingdomain.parking.vehicle.model.VehicleId
-import pl.cezarysanecki.parkingdomain.parking.vehicle.model.VehicleSize
 import pl.cezarysanecki.parkingdomain.parking.view.parkingspot.model.ParkingSpotViews
 import pl.cezarysanecki.parkingdomain.parking.view.vehicle.model.VehicleViews
-import pl.cezarysanecki.parkingdomain.reserving.parkingspot.application.ParkingSpotReservationsFinder
-import spock.lang.Specification
 
-@ActiveProfiles("local")
-@SpringBootTest(classes = [ParkingConfig.class, EventPublisherTestConfig.class])
-class AllowingToParkVehicleAcceptanceTest extends Specification {
+class AllowingToParkVehicleAcceptanceTest extends AbstractParkingAcceptanceTest {
   
-  @MockBean
-  ParkingSpotReservationsFinder parkingSpotReservationsFinder
-  
-  @Autowired
-  CreatingParkingSpot creatingParkingSpot
-  @Autowired
-  RegisteringVehicle registeringVehicle
   @Autowired
   ParkingVehicle parkingVehicle
   
@@ -60,17 +38,6 @@ class AllowingToParkVehicleAcceptanceTest extends Specification {
       vehiclesAreParkedOn(parkingSpotId, [firstVehicleId, secondVehicleId, thirdVehicleId])
     and:
       parkingSpotIsNotAvailable(parkingSpotId)
-  }
-  
-  ParkingSpotId createParkingSpot(int capacity) {
-    def result = creatingParkingSpot.create(new CreatingParkingSpot.Command(
-        ParkingSpotCapacity.of(capacity), ParkingSpotCategory.Gold))
-    return (result.get() as Result.Success<ParkingSpotId>).getResult()
-  }
-  
-  VehicleId registerVehicle(int size) {
-    def result = registeringVehicle.register(new RegisteringVehicle.Command(VehicleSize.of(size)))
-    return (result.get() as Result.Success<VehicleId>).getResult()
   }
   
   void vehiclesAreParkedOn(ParkingSpotId parkingSpotId, List<VehicleId> vehicles) {
