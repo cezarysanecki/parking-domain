a79532f8bb98fe0148bca78d39efe50a0d25c697
Cezary Sanecki <saneckicezary@gmail.com>
2024-04-17 16:06:24 +0200

    Added test for parking on reserved spot

---
+++ src/test/groovy/pl/cezarysanecki/parkingdomain/parking/vehicle/application/ParkingVehicleUsingReservationTest.groovy
@@ -0,0 +1,56 @@
+package pl.cezarysanecki.parkingdomain.parking.vehicle.application
+
+import io.vavr.control.Option
+import pl.cezarysanecki.parkingdomain.commons.commands.Result
+import pl.cezarysanecki.parkingdomain.parking.parkingspot.model.ParkingSpotId
+import pl.cezarysanecki.parkingdomain.parking.vehicle.model.Vehicles
+import pl.cezarysanecki.parkingdomain.reserving.client.model.ReservationId
+import pl.cezarysanecki.parkingdomain.reserving.parkingspot.application.ParkingSpotReservationsFinder
+import spock.lang.Specification
+import spock.lang.Subject
+
+import static pl.cezarysanecki.parkingdomain.parking.vehicle.application.ParkingVehicle.ParkOnReservedCommand
+import static pl.cezarysanecki.parkingdomain.parking.vehicle.model.VehicleFixture.notParkedVehicle
+
+class ParkingVehicleUsingReservationTest extends Specification {
+  
+  Vehicles vehicles = Mock()
+  ParkingSpotReservationsFinder parkingSpotReservationsFinder = Mock()
+  
+  ReservationId reservationId = ReservationId.newOne()
+  
+  @Subject
+  ParkingVehicle parkingVehicle = new ParkingVehicle(vehicles, parkingSpotReservationsFinder)
+  
+  def "allow to park vehicle on parking spot using reservation"() {
+    given:
+      def vehicle = notParkedVehicle()
+    and:
+      vehicles.findBy(vehicle.vehicleInformation.vehicleId) >> Option.of(vehicle)
+    and:
+      parkingSpotReservationsFinder.findParkingSpotIdBy(reservationId) >> Option.of(ParkingSpotId.newOne())
+    
+    when:
+      def result = parkingVehicle.park(new ParkOnReservedCommand(vehicle.vehicleInformation.vehicleId, reservationId))
+    
+    then:
+      result.isSuccess()
+      result.get() in Result.Success
+  }
+  
+  def "fail to park vehicle on parking spot when there is no parking spot linked with reservation"() {
+    given:
+      def vehicle = notParkedVehicle()
+    and:
+      vehicles.findBy(vehicle.vehicleInformation.vehicleId) >> Option.of(vehicle)
+    and:
+      parkingSpotReservationsFinder.findParkingSpotIdBy(reservationId) >> Option.none()
+    
+    when:
+      def result = parkingVehicle.park(new ParkOnReservedCommand(vehicle.vehicleInformation.vehicleId, reservationId))
+    
+    then:
+      result.isFailure()
+  }
+  
+}
