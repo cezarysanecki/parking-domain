1806937b4125bdba93cb5f47f8bd357fd2c5bb9f
Cezary Sanecki <saneckicezary@gmail.com>
2024-05-04 18:25:30 +0200

    Prepared new model

---
+++ src/main/java/pl/cezarysanecki/parkingdomain/parking/OccupationId.java
@@ -0,0 +1,21 @@
+package pl.cezarysanecki.parkingdomain.parking;
+
+import lombok.Value;
+
+import java.util.UUID;
+
+@Value(staticConstructor = "of")
+public class OccupationId {
+
+    UUID value;
+
+    public static OccupationId newOne() {
+        return new OccupationId(UUID.randomUUID());
+    }
+
+    @Override
+    public String toString() {
+        return value.toString();
+    }
+
+}
