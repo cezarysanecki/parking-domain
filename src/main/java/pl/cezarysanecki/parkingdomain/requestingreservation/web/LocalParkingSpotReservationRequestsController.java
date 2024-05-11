b0b1fa7cb9651268ab76044bb53b11108ac31a5e
Cezary Sanecki <saneckicezary@gmail.com>
2024-05-11 23:01:17 +0200

    Prepared endpoint for local testing

---
+++ src/main/java/pl/cezarysanecki/parkingdomain/requestingreservation/web/LocalParkingSpotReservationRequestsController.java
@@ -0,0 +1,31 @@
+package pl.cezarysanecki.parkingdomain.requestingreservation.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.cezarysanecki.parkingdomain.requestingreservation.application.MakingReservationRequestsValid;
+
+import java.util.List;
+
+@Profile("local")
+@RestController
+@RequestMapping("/requests")
+@RequiredArgsConstructor
+class LocalParkingSpotReservationRequestsController {
+
+    private final MakingReservationRequestsValid makingReservationRequestsValid;
+
+    @GetMapping("/make-valid")
+    ResponseEntity<List<String>> makeReservationRequestValid() {
+        io.vavr.collection.List<MakingReservationRequestsValid.Problem> result = makingReservationRequestsValid.makeValid();
+        if (result.isEmpty()) {
+            return ResponseEntity.ok(List.of());
+        }
+        return ResponseEntity.internalServerError()
+                .body(result.map(MakingReservationRequestsValid.Problem::reason).asJava());
+    }
+
+}
