02d3a9c4d956c38c5e6dcd964721ba7562ddecb6
Cezary Sanecki <saneckicezary@gmail.com>
2024-05-11 23:02:41 +0200

    Changed http method

--- src/main/java/pl/cezarysanecki/parkingdomain/requestingreservation/web/LocalParkingSpotReservationRequestsController.java
+++ src/main/java/pl/cezarysanecki/parkingdomain/requestingreservation/web/LocalParkingSpotReservationRequestsController.java
@@ -3,7 +3,7 @@ package pl.cezarysanecki.parkingdomain.requestingreservation.web;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.cezarysanecki.parkingdomain.requestingreservation.application.MakingReservationRequestsValid;
@@ -18,7 +18,7 @@ class LocalParkingSpotReservationRequestsController {
 
     private final MakingReservationRequestsValid makingReservationRequestsValid;
 
-    @GetMapping("/make-valid")
+    @PostMapping("/make-valid")
     ResponseEntity<List<String>> makeReservationRequestValid() {
         io.vavr.collection.List<MakingReservationRequestsValid.Problem> result = makingReservationRequestsValid.makeValid();
         if (result.isEmpty()) {
