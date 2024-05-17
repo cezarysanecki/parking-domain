fd6126e3d83a018fbbfdc028b12e9572efbc5b43
Cezary Sanecki <saneckicezary@gmail.com>
2024-05-17 14:06:46 +0200

    Added log

--- src/main/java/pl/cezarysanecki/parkingdomain/cleaning/application/RequestingCleaning.java
+++ src/main/java/pl/cezarysanecki/parkingdomain/cleaning/application/RequestingCleaning.java
@@ -1,21 +1,19 @@
 package pl.cezarysanecki.parkingdomain.cleaning.application;
 
-import io.vavr.control.Try;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import pl.cezarysanecki.parkingdomain.management.parkingspot.ParkingSpotId;
-import pl.cezarysanecki.parkingdomain.parking.application.OccupyingParkingSpot;
-import pl.cezarysanecki.parkingdomain.parking.model.beneficiary.BeneficiaryId;
-import pl.cezarysanecki.parkingdomain.parking.model.parkingspot.Occupation;
+import pl.cezarysanecki.parkingdomain.parking.application.ProvidingUsageOfParkingSpot;
 
+@Slf4j
 @RequiredArgsConstructor
 public class RequestingCleaning {
 
-    private static final BeneficiaryId CLEANING_SERVICE = BeneficiaryId.newOne();
-
-    private OccupyingParkingSpot occupyingParkingSpot;
+    private final ProvidingUsageOfParkingSpot providingUsageOfParkingSpot;
 
     public void requestCleaning(ParkingSpotId parkingSpotId) {
-        Try<Occupation> result = occupyingParkingSpot.occupyWhole(CLEANING_SERVICE, parkingSpotId);
+        log.debug("cleaning make parking spot with id {} of use", parkingSpotId);
+        providingUsageOfParkingSpot.makeOutOfUse(parkingSpotId);
     }
 
 }
