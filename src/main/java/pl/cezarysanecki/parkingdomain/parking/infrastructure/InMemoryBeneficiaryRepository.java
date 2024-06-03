7cc8bde98bfb1a0a447ce1f19d5d923156c6075c
Cezary Sanecki <saneckicezary@gmail.com>
2024-06-03 23:12:22 +0200

    Prepared database for beneficiary

--- src/main/java/pl/cezarysanecki/parkingdomain/parking/infrastructure/InMemoryBeneficiaryRepository.java
+++ src/main/java/pl/cezarysanecki/parkingdomain/parking/infrastructure/InMemoryBeneficiaryRepository.java
@@ -8,43 +8,17 @@ import pl.cezarysanecki.parkingdomain.parking.model.occupation.OccupationId;
 import pl.cezarysanecki.parkingdomain.parking.model.reservation.ReservationId;
 import pl.cezarysanecki.parkingdomain.parking.web.BeneficiaryViewRepository;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 class InMemoryBeneficiaryRepository implements
     BeneficiaryRepository,
     BeneficiaryViewRepository {
 
-  static final Map<BeneficiaryId, Beneficiary> DATABASE = new ConcurrentHashMap<>();
-
-  @Override
-  public void save(Beneficiary beneficiary) {
-    DATABASE.put(beneficiary.getBeneficiaryId(), beneficiary);
-  }
-
-  @Override
-  public Option<Beneficiary> findBy(BeneficiaryId beneficiaryId) {
-    return Option.of(DATABASE.get(beneficiaryId));
-  }
-
-  @Override
-  public Option<Beneficiary> findBy(OccupationId occupationId) {
-    return Option.ofOptional(
-        DATABASE.values()
-            .stream()
-            .filter(beneficiary -> beneficiary.getOccupations().contains(occupationId))
-            .findFirst());
-  }
-
-  @Override
-  public Option<Beneficiary> findBy(ReservationId reservationId) {
-    return Option.ofOptional(
-        DATABASE.values()
-            .stream()
-            .filter(beneficiary -> beneficiary.getReservations().contains(reservationId))
-            .findFirst());
-  }
+  static final Set<BeneficiaryId> DATABASE = new HashSet<>();
 
   @Override
   public List<BeneficiaryView> queryForAllBeneficiaries() {
@@ -56,5 +30,15 @@ class InMemoryBeneficiaryRepository implements
             beneficiary.getReservations().map(ReservationId::getValue).toJavaList()
         ))
         .toList();
+  }
+
+  @Override
+  public void save(BeneficiaryId beneficiaryId) {
+    DATABASE.add(beneficiaryId);
+  }
+
+  @Override
+  public boolean isPresent(BeneficiaryId beneficiaryId) {
+    return DATABASE.contains(beneficiaryId);
   }
 }
