6743a96e0a9c2a37c2e8a548e8d56e68771f80b8
Cezary Sanecki <saneckicezary@gmail.com>
2024-08-27 22:06:02 +0200

    Fixed problem with transactions

--- src/main/java/pl/cezarysanecki/parkingdomain/_global/DatabaseConfig.java
+++ src/main/java/pl/cezarysanecki/parkingdomain/_global/DatabaseConfig.java
@@ -6,10 +6,12 @@ import org.jooq.SQLDialect;
 import org.jooq.conf.RenderNameCase;
 import org.jooq.conf.RenderQuotedNames;
 import org.jooq.conf.Settings;
+import org.jooq.impl.DataSourceConnectionProvider;
 import org.jooq.impl.DefaultConfiguration;
 import org.jooq.impl.DefaultDSLContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.TransactionAwareDataSourceProxy;
 
 import javax.sql.DataSource;
 
@@ -24,10 +26,13 @@ class DatabaseConfig {
     Settings settings = new Settings()
         .withRenderQuotedNames(RenderQuotedNames.ALWAYS)
         .withRenderNameCase(RenderNameCase.LOWER);
+    DataSourceConnectionProvider dataSourceConnectionProvider = new DataSourceConnectionProvider(
+        new TransactionAwareDataSourceProxy(dataSource));
     return new DefaultDSLContext(new DefaultConfiguration()
         .set(dataSource)
         .set(settings)
-        .set(SQLDialect.POSTGRES));
+        .set(SQLDialect.POSTGRES)
+        .set(dataSourceConnectionProvider));
   }
 
 }
